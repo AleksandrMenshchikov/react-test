60a34e2a0bd4833b0cc8f29de7b5af48e2b3b803
Aleksandr Menshchikov <aleksandr.menshchikov1982@gmail.com>
2022-08-26 16:43:04 +0300

    refactoring

--- src/components/account/Account.tsx
+++ src/components/account/Account.tsx
@@ -1,8 +1,42 @@
 import styles from './Account.module.css';
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Box, Modal } from '@mui/material';
+import ModalMessage from '../modal-message/ModalMessage';
+import { useAppDispatch } from '../../redux/hooks';
+import { selectModal, setIsOpen } from '../../redux/features/modalSlice';
+import { selectUser } from '../../redux/features/userSlice';
+import { useSelector } from 'react-redux';
 
 function Account() {
-  return <div>Account</div>;
+  const { isOpen } = useSelector(selectModal);
+  const { user } = useSelector(selectUser);
+  const dispatch = useAppDispatch();
+
+  function handleModalClose() {
+    dispatch(setIsOpen(false));
+  }
+
+  useEffect(() => {
+    if (user) {
+      dispatch(setIsOpen(true));
+    }
+  }, []);
+
+  return (
+    <div>
+      <div>Account</div>
+      <Modal
+        open={isOpen}
+        onClose={handleModalClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box>
+          <ModalMessage />
+        </Box>
+      </Modal>
+    </div>
+  );
 }
 
 export default React.memo(Account);
